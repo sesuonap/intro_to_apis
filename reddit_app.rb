233f4c64a06e64618eda338478ab04baeff8ca52
sesuonap <tpanouses@gmail.com>
2018-05-03 21:32:01 -0500

    still working on reddit api

--- reddit_app.rb
+++ reddit_app.rb
@@ -1,2 +1,25 @@
 require 'unirest'
 
+puts "Welcome to the Terminal Reddit Browser"
+print "Please enter a subject you would like to visit: "
+
+subject = gets.chomp 
+
+response = Unirest.get("https://www.reddit.com/r/#{subject}.json")
+
+subreddit_title = response.body["data"]["children"][2]["data"]["subreddit"]
+subreddit_info = response.body["data"]["children"][2]["data"]["title"]
+
+
+
+subreddit_title = subreddit_title.upcase
+
+
+
+
+
+
+
+
+p "Currently viewing: /r/#{subreddit_title}"
+p subreddit_info 

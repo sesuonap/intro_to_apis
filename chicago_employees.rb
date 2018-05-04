88c8bea9b0ac45366e4169cb22076ea7aacfae89
sesuonap <tpanouses@gmail.com>
2018-05-03 20:24:56 -0500

    initial commit

---
+++ chicago_employees.rb
@@ -0,0 +1,11 @@
+ require 'unirest'
+
+ response = Unirest.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
+ 
+ data = response.body 
+
+ data.each do |employee_hash|
+  puts ""
+  puts "===================="
+  puts "#{employee_hash["name"]} - #{employee_hash["job_titles"]}"
+end 
